a312705f381a61bafb386254b03b2a3d7e37fbcf
Josephine Mellor <josephinemellor10@gmail.com>
2025-10-01 22:17:43 +0100

    different magnetic field defininition

--- C++_scripts/SLIC_w_magnetic_potential.cpp
+++ C++_scripts/SLIC_w_magnetic_potential.cpp
@@ -1360,7 +1360,7 @@ void thermalSourceTerm_explicit_RK4(big_array& u, double dt, double t, double dx
 
 int main() { 
     clock_t start = clock();
-    int nCells = 400; //the distance between points is 0.01
+    int nCells = 200; //the distance between points is 0.01
     double x0 = 0.0;
     double x1 = 0.2;
     double tStart = 0.0; //set the start and finish time steps the same
